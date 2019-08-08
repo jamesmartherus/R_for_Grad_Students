c480dbdaf3126ecf01f8f55c28617f3d92c658b5
jamesmartherus <james@martherus.com>
2019-08-08 11:36:15 -0500

    Add Day 5 Materials

---
+++ Day5_Exercises/1_ForLoops.R
@@ -0,0 +1,16 @@
+###########
+# For Loops
+#
+###########
+
+
+# Exercise 1: 
+# Use a for loop to take the square root of each value in the following vector:
+# `vec1 <- c(4, 9, 81, 100, 1000, 10^6)`. Save the results to a new vector called `vec2`.
+
+
+# Exercise 2:
+# Import the animals dataset.
+# Use a for loop to add "_var" to the names of the predator, aquatic, and airborne variables.
+# Hint 1: first make a vector with those variable names and then use that in your for loop instead
+# of numbers, e.g. `for(i in your_vector)``
